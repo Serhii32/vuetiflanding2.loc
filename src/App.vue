4189b6b7daaf880034be9643adb542a9dad0faac
vagrant <vagrant@homestead>
2019-08-06 14:18:44 +0000

    5 commit

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,60 @@
 <template>
   <v-app>
-    <v-app-bar app flat :color="color">
+    <v-app-bar v-scroll="onScroll" app flat :color="menuBGColor">
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetifyd</span>
+        <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Releasefff</span>
       </v-btn>
     </v-app-bar>
-
     <v-content class="pa-0">
       <Index/>
     </v-content>
+    <v-footer dark padless>
+      <v-card class="flex" flat tile>
+        <v-card-title class="teal">
+          <strong class="subheading">Get connected with us on social networks!</strong>
+          <v-spacer></v-spacer>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-card-actions class="grey darken-3 justify-center">
+          {{ new Date().getFullYear() }} — <strong>Doors</strong>
+        </v-card-actions>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 import Index from './components/Index';
-
 export default {
   name: 'App',
   components: {
     Index,
   },
   data: () => ({
-    color: "transparent"
+    menuBGColor: null,
+    icons: [
+      'fab fa-facebook',
+      'fab fa-twitter',
+      'fab fa-google-plus',
+      'fab fa-linkedin',
+      'fab fa-instagram',
+    ],
   }),
+  mounted() {
+    this.onScroll()
+  },
+  methods: {
+    onScroll() {
+      this.menuBGColor = window.pageYOffset > 0 ? 'white' : 'transparent'
+    }
+  }
 };
 </script>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,63 +2,45 @@
 	<v-container fluid pa-0>
 		<v-layout wrap>
 			<v-img width="100%" height="100vh" :src="require('../assets/first-block.jpg')">
-				
+				<v-layout>
+					<v-flex xs12 sm6>
+						<div class="title font-weight-bold text-uppercase ma-auto">Межкомнатные двери в Виннице - более 600 моделей в наличии в магазине</div>
+					</v-flex>
+					<v-flex xs12 sm6>
+						
+					</v-flex>
+				</v-layout>
 			</v-img>
 		</v-layout>
 		<v-layout wrap>
 			<video-player  class="vjs-custom-skin" :options="playerOptions" :playsinline="true"></video-player>
 		</v-layout>
-		<v-layout wrap>
-			<v-tabs>
-				<v-tab v-for="(doorType, i) in doorTypes" :key="i" :href="'#tab-' + i">{{ doorType.name }}</v-tab>
-					<v-tab-item v-for="(doorType, i) in doorTypes" :key="i" :value="'tab-' + i">{{ doorType.description }}</v-tab-item>
+		<v-layout wrap class="pa-10">
+			<div class="title font-weight-bold text-uppercase ma-auto">лидеры продаж</div>
+			<v-tabs centered :vertical="$vuetify.breakpoint.xsOnly">
+				<v-tab v-for="(doorType, i) in doorTypes" :key="doorType.name + '_' + i">{{ doorType.name }}</v-tab>
+				<v-tab-item v-for="(doorType, i) in doorTypes" :key="doorType.name + '_' + i">					
+					<swiper :options="swiperOption">
+						<swiper-slide v-for="(door, i) in doorType.doors" :key="door.name + '_' + i">
+							<v-card width="200px" height="500px" class="ma-auto pa-2" flat>
+								<img :src="door.photo" height="400">
+								<v-card-title  primary-title>{{door.name}}</v-card-title>
+								<v-card-text>{{door.price}}</v-card-text>
+							</v-card>
+						</swiper-slide>
+						<div class="swiper-pagination" slot="pagination"></div>
+					</swiper>
+				</v-tab-item>
 			</v-tabs>
 		</v-layout>
-		<v-layout wrap>
-			<swiper :options="swiperOption">
-				<swiper-slide>Slide 1</swiper-slide>
-				<swiper-slide>Slide 2</swiper-slide>
-				<swiper-slide>Slide 3</swiper-slide>
-				<swiper-slide>Slide 4</swiper-slide>
-				<swiper-slide>Slide 5</swiper-slide>
-				<swiper-slide>Slide 6</swiper-slide>
-				<swiper-slide>Slide 7</swiper-slide>
-				<div class="swiper-pagination" slot="pagination"></div>
-			</swiper>
-		</v-layout>
-		<v-layout wrap>
-			<v-tabs>
-				<v-tab v-for="(uniqueDoor, i) in uniqueDoors" :key="i" :href="'#tab-' + i">{{ uniqueDoor.name }}</v-tab>
-					<v-tab-item v-for="(uniqueDoor, i) in uniqueDoors" :key="i" :value="'tab-' + i">{{ uniqueDoor.description }}</v-tab-item>
-			</v-tabs>
-		</v-layout>
-		<v-layout wrap align-center justify-center fill-height>
-			<v-flex xs12 md4>
-				<v-hover v-slot:default="{ hover }">
-					<v-card width="100%" height="500px">
-						<v-card-media :src="require('../assets/first-block.jpg')" height="200px"></v-card-media>
-						<v-card-title  primary-title>I'm a title</v-card-title>
-						<v-card-text>I'm card text</v-card-text>
-					</v-card>
-				</v-hover>
-			</v-flex>
-			<v-flex xs12 md4>
-				<v-hover v-slot:default="{ hover }">
-					<v-card width="100%" height="500px">
-						<v-card-media :src="require('../assets/first-block.jpg')" height="200px"></v-card-media>
-						<v-card-title  primary-title>I'm a title</v-card-title>
-						<v-card-text>I'm card text</v-card-text>
-					</v-card>
-				</v-hover>
-			</v-flex>
-			<v-flex xs12 md4>
-				<v-hover v-slot:default="{ hover }">
-					<v-card width="100%" height="500px">
-						<v-card-media :src="require('../assets/first-block.jpg')" height="200px"></v-card-media>
-						<v-card-title  primary-title>I'm a title</v-card-title>
-						<v-card-text>I'm card text</v-card-text>
-					</v-card>
-				</v-hover>
+		<v-layout class="pa-10" wrap align-center justify-center fill-height>
+			<v-flex xs12 class="title font-weight-bold text-uppercase text-center">наши преимущества</v-flex>
+			<v-flex xs12 md4 v-for="(advantage, i) in advantages" :key="advantage.title + '_' + i">
+				<v-card style="max-width: 400px; min-height: 350px;" width="90%" class="ma-auto">
+					<v-img :src="advantage.photo" height="200px"></v-img>
+					<v-card-title  primary-title class="text-center">{{advantage.title}}</v-card-title>
+					<v-card-text>{{advantage.description}}</v-card-text>
+				</v-card>
 			</v-flex>
 		</v-layout>
 	</v-container>
@@ -70,44 +52,111 @@
 				doorTypes: [
 					{
 						name: 'Деревянные',
-						description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+						doors: [
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+						]
 					},
 					{
 						name: 'МДФ',
-						description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+						doors: [
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+						]
 					},
 					{
 						name: 'Шпонированные',
-						description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+						doors: [
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+						]
 					},
 					{
 						name: 'Под покраску',
-						description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-					}
-				],
-				uniqueDoors: [
-					{
-						name: 'Деревянные',
-						description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-					},
-					{
-						name: 'МДФ',
-						description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-					},
-					{
-						name: 'Шпонированные',
-						description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-					},
-					{
-						name: 'Под покраску',
-						description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+						doors: [
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+							{
+								name: 'Дверь',
+								photo: require('../assets/door.jpg'),
+								price: '1000'
+							},
+						]
 					}
 				],
 				swiperOption: {
 					effect: 'coverflow',
-					grabCursor: true,
+					grabCursor: false,
 					centeredSlides: true,
-					slidesPerView: 'auto',
+					slidesPerView: 3,
+					loop: true,
 					coverflowEffect: {
 						rotate: 50,
 						stretch: 0,
@@ -116,25 +165,45 @@
 						slideShadows : true
 					},
 					pagination: {
-						el: '.swiper-pagination'
+						el: '.swiper-pagination',
+						clickable: true
 					}
 				},
+				advantages: [
+					{
+						title: 'Покупай модное',
+						description: 'Мы всегда в курсе актуальных тенденций мирового дизайна, которые представлены в нашых шоу-румах',
+						photo: require('../assets/1.png'),
+					},
+					{
+						title: 'Прямые поставки',
+						description: 'Фирменные магазины Папа Карлов и фабрики Wakewood',
+						photo: require('../assets/2.jpg'),
+					},
+					{
+						title: 'Воплощаем идеи',
+						description: 'Изготовим продукцию по индивидуальным параметрам и габаритам',
+						photo: require('../assets/3.jpg'),
+					},
+				],
 				playerOptions: {
-					height: '360',
+					// height: '100vh',
+					// width: '100%',
 					autoplay: true,
 					muted: true,
+					responsive: true,
 					language: 'ru',
 					playbackRates: [0.7, 1.0, 1.5, 2.0],
 					sources: [{
-					type: "video/mp4",
-					// mp4
-					src: "http://vjs.zencdn.net/v/oceans.mp4",
-					// webm
-					// src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+						type: "video/mp4",
+						// mp4
+						src: "http://vjs.zencdn.net/v/oceans.mp4",
+						// webm
+						// src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
 					}],
 					poster: "https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg",
 				}
 			}
-		},
+		}
 	}
 </script>
